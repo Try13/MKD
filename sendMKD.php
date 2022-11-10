1efaadb4006e52f35b666fe135f78f66dcac49d4
Try13 <110610195+Try13@users.noreply.github.com>
2022-11-10 17:05:48 +0300

    Delete sendMKD.php

--- sendMKD.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-$userName = $_POST["user-name"];
-$userNamber = $_POST["user-namber"];
-
-mail('review@mebelcomfortdon.ru', 'Письмо с сайта', "$userName", "$userNamber");
